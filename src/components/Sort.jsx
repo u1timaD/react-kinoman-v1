ec710df8d65bed1e008d0c3dd2b3070b057ec8e0
Dmitry <free-doom46@mail.ru>
2023-12-07 00:34:59 +0300

    feat: add sort map render

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 function Sort() {
+  const sortType = ["default", "date", "rating"];
+	const [sortActive, setSortActive] = useState(0);
+
   return (
     <ul className="sort">
-      <li>
+      {sortType.map((type, index) => (
+        <li key={index}>
+          <a href="#" className={`sort__button ${sortActive === index ? 'sort__button--active' : ''}`} onClick={() => setSortActive(index)}>
+            {`Sort by ${type}`}
+          </a>
+        </li>
+      ))}
+      {/* <li>
         <a href="#" className="sort__button sort__button--active">
           Sort by default
         </a>
@@ -15,7 +27,7 @@ function Sort() {
         <a href="#" className="sort__button">
           Sort by rating
         </a>
-      </li>
+      </li> */}
     </ul>
   );
 }
